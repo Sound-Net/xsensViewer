edd49363a875fe4b7219a2ec6cf671ceb69bffb1
macst <macst@192.168.1.102>
2018-04-08 10:58:22 +0200

    Fixed bad merge

--- xsensViewer/src/comms/SerialUtils.java
+++ xsensViewer/src/comms/SerialUtils.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package comms;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
@@ -36,42 +35,3 @@ public class SerialUtils {
 	}
 
 }
-=======
-package comms;
-
-import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
-import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
-import org.apache.commons.math3.geometry.euclidean.threed.RotationOrder;
-
-public class SerialUtils {
-
-	/**
-	 * 
-	 */
-	public static Integer[]  baudRate={110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000 };
-
-
-	/**
-	 * Calculate euler angles from a quaternion
-	 * @param quaternion - a quaternion
-	 * @return euler angles in RADIANS
-	 */
-	public static double[] quat2Eul(double[] quaternion){
-
-		Rotation rotation = new Rotation(quaternion[0], quaternion[1], 
-				quaternion[2], quaternion[3],false); 
-
-		try{
-			double[] angles = rotation.getAngles(RotationOrder.ZYX,  RotationConvention.FRAME_TRANSFORM);
-			return angles;
-		}
-
-		catch (Exception e){
-			System.err.println("Currupt Quaternion: Could not convert to Euler");
-			return new double[4]; 
-		}
-
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/macster110/xsensViewer.git
